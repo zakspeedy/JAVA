d37d8d8f3c4b5d4e7213588d1c4f25ed0f532018
zakspeedy <123161584+zakspeedy@users.noreply.github.com>
2023-02-06 15:47:58 +0530

    Add files via upload

---
+++ practical/Function.java
@@ -0,0 +1,13 @@
+class My{
+public static void Display()
+{
+System.out.println("apple mango");
+}
+}
+class function{
+public static void main(String args[])
+{
+System.out.println("hiii im at vashi ");
+My.Display();
+}
+}
